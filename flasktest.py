1950ba2400d197159bf34162bc2e1b7fe15e06a3
Pranav Reddy Charkupalli <pranavcharku07@gmail.com>
2020-09-20 23:50:29 -0500

    Added Personality Buttons
    Put personality test buttons on the webpage that would run the testing scripts

--- flasktest.py
+++ flasktest.py
@@ -8,18 +8,22 @@ def bruh():
     if request.method == 'POST':
         user = request.form["username"]
         password = request.form["password"]
-        return redirect(url_for("send", usr = user, word = password))
+        return redirect(url_for("send", mbti = "?", dnd = "?", types = "?", usr = user, word = password))
     else:
         return render_template("root.html")
 
-@app.route('/login-<usr>-<word>')
-def send(usr, word):
-    return render_template("login.html", usr = usr, word = word)
+@app.route('/login-<usr>-<word>-<mbti>-<dnd>-<types>', methods=['GET', 'POST'])
+def send(mbti, dnd, types, usr, word):
+    mb = mbti
+    dn = dnd
+    typ = types
+    if request.method == 'POST':
+        mb = functions.detect(0)
+    return render_template("login.html", mbti = mb, dnd = dn, type = typ, usr = usr, word = word)
 
-@app.route("/<name>")
-def rando(name):
-    return f"Hello {name}!"
-
+# @app.route("/<name>")
+# def rando(name):
+#     return f"Hello {name}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
